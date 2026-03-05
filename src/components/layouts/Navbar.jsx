e3e3a9134f37863a088b34a0b8f0011bd6d924c3
Sakshi <sakshij4869005@gmail.com>
2026-03-05 19:05:27 +0530

    add media query for navbar

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from "react"
 const Navbar = ({ visible = true }) => {
     return (
-        <nav className={`hidden xl:block absolute left-1/2 -translate-x-[37%] bottom-[-30px] w-[75%] z-30 transition-all duration-500 ${visible ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-4 pointer-events-none'}`}>
+        <nav className={`hidden xl:block absolute left-1/2 -translate-x-[37%] bottom-[-38px] w-[80%] z-30 transition-all duration-500 ${visible ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-4 pointer-events-none'}`}>
 
-            <div className="bg-gradient-to-r from-[#1F2F5E] to-[#5973b1] text-white px-20 py-4 shadow-lg nav-diagonal flex items-center justify-between">
+            <div className="bg-gradient-to-r from-[#1F2F5E] to-[#5973b1] text-white px-10 py-2 shadow-lg nav-diagonal flex items-center justify-between">
 
-                <ul className="flex items-center gap-14 text-lg font-medium tracking-wider cursor-pointer">
+                <ul className="flex items-center gap-10 text-[14px] font-medium tracking-wider cursor-pointer">
                     <li><a href="#" className="hover:text-[var(--viva-gold)] transition">About Us</a></li>
                     <li><a href="#" className="hover:text-[var(--viva-gold)] transition">Programmes</a></li>
                     <li><a href="#" className="hover:text-[var(--viva-gold)] transition">Admission</a></li>
